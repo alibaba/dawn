38c8f757c91050cba04657dd13a50936d7711d8d
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2017-08-30 01:17:45 +0800

    [modify] update test cases

--- test/cli-init.test.js
+++ test/cli-init.test.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 describe('cli', function () {
 
   it('#init', async function () {
+    this.timeout(12000);
     process.chdir(path.resolve(__dirname, './demo2'));
     let dirname = path.resolve(__dirname, './demo2/.dawn/');
     let filename = path.resolve(__dirname, './demo2/.dawn/pipe.yml');
@@ -12,7 +13,7 @@ describe('cli', function () {
     if (fs.existsSync(dirname)) fs.rmdirSync(dirname);
     expect(fs.existsSync(filename)).to.be.equal(false);
     cli.parse(['node', 'dn', 'init', '-t', 'dn-template-unit-demo']);
-    await sleep(3000);
+    await sleep(10000);
     expect(fs.existsSync(filename)).to.be.equal(true);
   });
 
