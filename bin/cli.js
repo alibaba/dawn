91940b10b357638a338c20e8ce8f43524b1dbf65
Houfeng <admin@xhou.net>
2017-09-14 20:30:46 +0800

    [modify] add upgrade check

--- bin/cli.js
+++ bin/cli.js
@@ -10,22 +10,12 @@ const pkg = require('../package.json');
 const semver = require('semver');
 const debug = require('debug')('cli');
 
-(async function () {
+debug('process.version', process.version);
+debug('pkg.engines.node', pkg.engines.node);
+if (!semver.satisfies(process.version, pkg.engines.node)) {
+  return console.error([`The Node version requirement is ${pkg.engines.node}`,
+  `but the current version is ${process.version}`].join(', '));
+}
 
-  debug('process.version', process.version);
-  debug('pkg.engines.node', pkg.engines.node);
-  if (!semver.satisfies(process.version, pkg.engines.node)) {
-    return console.error([`The Node version requirement is ${pkg.engines.node}`,
-    `but the current version is ${process.version}`].join(', '));
-  }
-
-  let cliCore = require('./cli-core');
-  let upgrade = require('../lib/upgrade');
-
-  debug('upgrade.check')
-  await upgrade.check();
-
-  debug('process.argv', process.argv);
-  cliCore.ready();
-
-})();
+debug('process.argv', process.argv);
+require('./cli-core').ready();
