c9a2ef6c92c7e6dbea23abf55213b12bb151ce43
Houfeng <admin@xhou.net>
2017-09-29 09:57:03 +0800

    0.7.8

--- bin/cli.js
+++ bin/cli.js
@@ -5,7 +5,6 @@
  * @author Houfeng <admin@xhou.net>
  */
 
-const console = require('console3');
 const pkg = require('../package.json');
 const semver = require('semver');
 const debug = require('debug')('cli');
@@ -13,6 +12,7 @@ const debug = require('debug')('cli');
 debug('process.version', process.version);
 debug('pkg.engines.node', pkg.engines.node);
 if (!semver.satisfies(process.version, pkg.engines.node)) {
+  let console = require('console3');
   return console.error([`The Node version requirement is ${pkg.engines.node}`,
   `but the current version is ${process.version}`].join(', '));
 }
