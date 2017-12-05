2483131699c859367f6ff9f2c58a4bab5d17ca70
Houfeng <admin@xhou.net>
2017-12-06 01:55:45 +0800

    call 中间件支持按条件调用

--- packages/dn-middleware-call/lib/index.js
+++ packages/dn-middleware-call/lib/index.js
@@ -5,16 +5,44 @@
  */
 module.exports = function (opts) {
 
+  opts = Object.assign({}, opts);
+
   //外层函数的用于接收「参数对象」
   //必须返回一个中间件处理函数
   return async function (next) {
 
-    if (!opts.pipe) return next();
+    if (!opts.pipe && !opts.when) return next();
 
-    //在这里处理你的逻辑
-    this.console.info(`Calling '${opts.pipe}'`);
-    await this.exec(await this.loadPipeline(opts.pipe));
-    this.console.info('Done');
+    if (!this.utils.isArray(opts.pipe)) {
+      opts.pipe = [opts.pipe];
+    }
+
+    let call = async pipes => {
+      for (let i = 0; i < pipes.length; i++) {
+        let pipe = pipes[i];
+        let list = await this.loadPipeline(pipe);
+        if (!list || list.length < 1) continue;
+        this.console.info(`Calling '${pipe}'`);
+        await this.exec(list);
+      }
+    };
+
+    let compileCache = {};
+    let compile = function (expr) {
+      if (!compileCache[expr]) {
+        compileCache[expr] = new Function(`return (${expr})`);
+      }
+      return compileCache[expr];
+    };
+
+    let list = [...opts.pipe];
+
+    this.utils.each(opts.when, (expr, pipe) => {
+      let func = compile(expr);
+      if (func.call(this)) list.push(pipe);
+    });
+
+    await call(list);
 
     //next 触发后续执行
     //如果需要在后续中间件执行完成再做一些处理
