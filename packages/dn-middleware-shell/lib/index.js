6e50c8c12c60a33db7345929db01ffb17528d24b
Houfeng <admin@xhou.net>
2017-12-21 23:53:25 +0800

    0.9.2 改进 this.utils.exec 方法，可直接使用本地依赖包中的命令

--- packages/dn-middleware-shell/lib/index.js
+++ packages/dn-middleware-shell/lib/index.js
@@ -1,11 +1,15 @@
 const os = require('os');
+const path = require('path');
 
 module.exports = function (opts) {
   let script = opts.script || []
   return async function (next) {
     this.console.info('Execute script...');
     try {
-      await this.utils.exec(script.join(os.EOL));
+      const env = Object.assign({}, process.env);
+      const bin = path.normalize(`${this.cwd}/node_modules/.bin`);
+      env.PATH = `${env.PATH}:${bin}`;
+      await this.utils.exec(script.join(os.EOL), { env });
       this.console.info('Done');
       next();
     } catch (err) {
