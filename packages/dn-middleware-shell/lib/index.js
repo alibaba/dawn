11d8e6fed51b46f3285c065b03ae2aaa7cedc37e
Houfeng <admin@xhou.net>
2018-01-31 15:51:18 +0800

    dn-middleware-shell 1.1.0

--- packages/dn-middleware-shell/lib/index.js
+++ packages/dn-middleware-shell/lib/index.js
@@ -1,15 +1,19 @@
 const os = require('os');
 const path = require('path');
 
+const isWin = os.platform() == 'win32';
+
 module.exports = function (opts) {
-  let script = opts.script || []
+  let script = isWin && opts.wscript ? opt.wscript : opts.script;
+  script = script || [];
   return async function (next) {
     this.console.info('Execute script...');
     try {
       const env = Object.assign({}, process.env);
       const bin = path.normalize(`${this.cwd}/node_modules/.bin`);
-      env.PATH = `${env.PATH}:${bin}`;
-      await this.utils.exec(script.join(os.EOL), { env });
+      env.PATH = `${env.PATH}${isWin ? ';' : ':'}${bin}`;
+      const pending = this.utils.exec(script.join(os.EOL), { env });
+      if (!opts.async) await pending;
       this.console.info('Done');
       next();
     } catch (err) {
