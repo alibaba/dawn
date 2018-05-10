6b53ebac4e019c313c89ea5156d37a864ec30b0c
Houfeng <admin@xhou.net>
2018-05-10 15:51:14 +0800

    更新 e2e 示例

--- packages/dn-middleware-e2e/test/e2e/baidu.test.js
+++ packages/dn-middleware-e2e/test/e2e/baidu.test.js
@@ -3,12 +3,12 @@ import assert from 'assert';
 describe('demo test', function () {
 
   beforeEach(async function () {
-    await driver.get('http://localhost:8000/');
+    await driver.get('https://www.baidu.com');
   });
 
   it('check title', async function () {
     let title = await driver.getTitle();
-    expect(title).to.equal('Not Found (404)');
+    expect(title).to.equal('百度一下，你就知道');
   });
 
 });
