72bee91925ed115ae084180893186b055e06261a
Houfeng <admin@xhou.net>
2018-08-24 15:49:28 +0800

    [modify] 所有不可变变量调用为 const & 处理选择最大列字符数 & fetch 容错

---
+++ packages/dn-middleware-typedoc/lib/index.js
@@ -0,0 +1,23 @@
+/**
+ * 这是一个标准的中间件工程模板
+ * @param {object} opts cli 传递过来的参数对象 (在 pipe 中的配置)
+ * @return {AsyncFunction} 中间件函数
+ */
+module.exports = function (opts) {
+
+  //外层函数的用于接收「参数对象」
+  //必须返回一个中间件处理函数
+  return async function (next) {
+
+    //在这里处理你的逻辑
+    this.console.log('This is an example');
+    this.console.log('opts', JSON.stringify(opts));
+
+    //next 触发后续执行
+    //如果需要在后续中间件执行完成再做一些处理
+    //还可以 await next(); 并在之后添加逻辑
+    next();
+
+  };
+
+};
