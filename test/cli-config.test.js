46eb2a5f811228287daddd9653307c0c8db556a8
Houfeng <admin@xhou.net>
2018-12-13 09:46:20 +0800

    modify: 重构单元测试

--- test/cli-config.test.js
+++ test/cli-config.test.js
@@ -7,13 +7,13 @@ describe('cli', function () {
   it('#config', function (done) {
     let ttl1 = Date.now();
     cli.once('done', () => {
-      configs.getRc('cacheTTL').then(ttl2 => {
+      configs.getRc('cache').then(ttl2 => {
         expect(ttl2).to.be.equal(ttl1);
-        configs.setLocalRc('cacheTTL', 0);
+        configs.setLocalRc('cache', 0);
         done();
       });
     });
-    cli.parse(['node', 'dn', 'config', 'cacheTTL', ttl1]);
+    cli.parse(['node', 'dn', 'config', 'cache', ttl1]);
   });
 
 });
