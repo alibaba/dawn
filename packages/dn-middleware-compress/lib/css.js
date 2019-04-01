c2492d6d1c9745767f2b19e5130b5412e16fb6a8
Houfeng <admin@xhou.net>
2019-04-01 12:07:43 +0800

    dn-middleware-compress@1.0.3

--- packages/dn-middleware-compress/lib/css.js
+++ packages/dn-middleware-compress/lib/css.js
@@ -1,10 +1,9 @@
 const CleanCSS = require('clean-css');
 const fs = require('mz/fs');
 
-const cleanCss = new CleanCSS();
-
-module.exports = async (file) => {
+module.exports = async (file, opts) => {
   const source = (await fs.readFile(file)).toString();
+  const cleanCss = new CleanCSS(opts);
   const { errors, styles } = cleanCss.minify(source);
   if (errors && errors[0]) throw new Error(errors[0]);
   await fs.writeFile(file, styles);
