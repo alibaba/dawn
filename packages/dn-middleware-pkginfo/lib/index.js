fbf0a05adfc4cb7a32d47317406ab6b0b50bb757
borrain <weirui.hwr@alibaba-inc.com>
2019-05-17 10:32:52 +0800

    feat:pkInfo加入endpoint

--- packages/dn-middleware-pkginfo/lib/index.js
+++ packages/dn-middleware-pkginfo/lib/index.js
@@ -21,11 +21,12 @@ module.exports = function (opts) {
     if (!this.inquirer) {
       throw new Error('请升级 dawn 到最新版本');
     }
+    const defaultPkgName = (opts && opts.endpoint || '') + path.basename(this.cwd);
 
     opts.items = opts.items || [{
       name: 'name',
       type: 'input',
-      default: path.basename(this.cwd),
+      default: defaultPkgName,
       message: '请输入项目名称',
       validate: function (name) {
         return !!name;
