b61f0d1734095d9690fcfc4fdc8378081ba5cca5
Houfeng <admin@xhou.net>
2019-08-13 16:29:40 +0800

    dn-middleware-doc@2.1.1

--- packages/dn-middleware-doc/lib/index.js
+++ packages/dn-middleware-doc/lib/index.js
@@ -3,7 +3,7 @@ module.exports = function (opts) {
     const doc = this.utils.findCommand(__dirname, 'doczilla');
     opts = Object.assign({ mode: process.env.DN_CMD || 'build' }, opts);
     const command = `${doc} ${opts.mode}`;
-    this.utils.exec(command);
+    await this.utils.exec(command);
     next();
   };
 };
