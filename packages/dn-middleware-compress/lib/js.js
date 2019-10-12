37a9c61956400156c1dce9cf34f4e8fec6dfa482
Houfeng <admin@xhou.net>
2019-10-12 15:31:37 +0800

    JS 压缩支持使用 terser

--- packages/dn-middleware-compress/lib/js.js
+++ packages/dn-middleware-compress/lib/js.js
@@ -1,9 +1,12 @@
 const UglifyJS = require("uglify-js");
+const Terser = require('terser')
 const fs = require('mz/fs');
 
 module.exports = async (file, opts) => {
+  const { terser, ...otherOpts } = Object.assign({}, opts);
+  const compresser = opts.terser ? Terser : UglifyJS;
   const source = (await fs.readFile(file)).toString();
-  const { error, code } = UglifyJS.minify(source, opts);
+  const { error, code } = compresser.minify(source, otherOpts);
   if (error) throw new Error(error);
   await fs.writeFile(file, code);
 };
