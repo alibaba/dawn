26aedcd446f5887c3fa967a9e65a77bd8c753e81
Jeason <jitong.zjt@alibaba-inc.com>
2020-04-26 18:36:00 +0800

    feat: exec

--- packages/dawn/src/commands/exec.ts
+++ packages/dawn/src/commands/exec.ts
@@ -5,21 +5,11 @@ export default class Exec extends Command {
 
   static flags = {
     help: flags.help({ char: "h" }),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: "n", description: "name to print" }),
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: "f" }),
   };
-
-  static args = [{ name: "file" }];
+  static strict = false;
 
   async run() {
-    const { args, flags } = this.parse(Exec);
-
-    const name = flags.name || "world";
-    this.log(`hello ${name} from /Users/jeason/projects/github.com/alibaba/dawn/packages/dawn/src/commands/exec.ts`);
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`);
-    }
+    const options = this.parse(Exec);
+    console.log(options);
   }
 }
