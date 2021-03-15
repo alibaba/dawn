58f1c6f915a0b3e3cb7203a0af0fe65718a06e6d
soulwu <soulwuzjjx@vip.qq.com>
2021-03-15 11:29:13 +0800

    init v2_dev

--- packages/dn-middleware-webpack-happy/build/js/index.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0],[function(o,e,n){o.exports=n(1)},function(o,e){function n(){console.log("你好中国")}e.__esModule=!0,e.default=n,o.exports=e.default}],[0]);
-//# sourceMappingURL=index.js.map
