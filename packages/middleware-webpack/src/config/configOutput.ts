e46d6e09a02ad4215cf9403198351016443eefd4
soulwu <longgui.wjb@alibaba-inc.com>
2021-06-22 14:42:23 +0800

    fix(@dawnjs/dn-middleware-webpack): not set globalObject by default

--- packages/middleware-webpack/src/config/configOutput.ts
+++ packages/middleware-webpack/src/config/configOutput.ts
@@ -8,7 +8,6 @@ export default async (config: Config, options: INormalizedOpts) => {
     .chunkFilename(path.join(options.folders.script, "[name].[chunkhash:8].chunk.js"))
     .set("assetModuleFilename", path.join(options.folders.media, "[name].[contenthash:8][ext]")) // TODO: replace after webpack-chain support webpack5
     .publicPath(options.publicPath ?? (options.env === "development" ? "/" : undefined))
-    .globalObject("this")
     .merge(options.output)
     .path(path.resolve(options.cwd, options.output.path));
 };
