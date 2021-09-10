b39858faa71a21cc5283718b400718eb890dea98
soulwu <longgui.wjb@alibaba-inc.com>
2021-09-10 11:03:25 +0800

    fix(@dawnjs/cli): fix async readJson

--- packages/cli/lib/common/readJsonSync.js
+++ packages/cli/lib/common/readJsonSync.js
@@ -3,7 +3,7 @@ const console = require('console');
 
 module.exports = function (filename) {
   try {
-    return JSON.parsr(fs.readFileSync(filename).toString('utf-8'));
+    return JSON.parse(fs.readFileSync(filename).toString('utf-8'));
   } catch (e) {
     console.error(e);
     return {};
